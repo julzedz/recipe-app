d3e996d494557398da60bd4afb2f6ff3f18a5617
julzedz <edoziechukwuma@gmail.com>
2023-08-01 23:41:56 +0100

    Add devise parameters to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :food
   has_many :recipe
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 end
