350af09108b7177ccea01b8578faface9523fc37
Ellyboi <vitoesitemitope@gmail.com>
2023-08-05 17:33:48 +0100

    working on rspec

--- db/migrate/20230802154254_add_user_to_food.rb
+++ db/migrate/20230802154254_add_user_to_food.rb
@@ -1,5 +1,5 @@
 class AddUserToFood < ActiveRecord::Migration[7.0]
   def change
-    add_reference :foods, :user, foreign_key: true
+    add_reference :foods, :user, foreign_key: true unless column_exists?(:foods, :user_id)
   end
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   before(:each) do
-    @user = User.create(name: 'Julian')
+    @user = User.create(name: 'Burger')
     @food = Food.new(name: 'Pizza', user_id: @user.id, measurement_unit: 'grams', price: 10, quantity: 1)
   end
 
