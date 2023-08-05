af6df329ad9f5bc04549cf2f2f986b718a0f3e43
julzedz <edoziechukwuma@gmail.com>
2023-08-05 03:42:00 +0100

    Add improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
   has_many :recipes, dependent: :destroy
 
