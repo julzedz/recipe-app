6367abc621b1e1ff22f1c7f220f70fefd070ef89
julzedz <edoziechukwuma@gmail.com>
2023-08-05 01:56:20 +0100

    Add validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id', optional: true
 
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   def total_price
